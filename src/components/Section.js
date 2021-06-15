f86a0c9abb42a28e0ca6992d32f7f5a917808b23
Maksym <maksym.sivko@gmail.com>
2021-06-15 14:50:29 +0300

    +

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,20 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
 
-const Wrap = styled.div`
-  width: 100vw;
-  height: 100vh;
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  background-image: url('/images/model-s.jpg');
-`;
-
-const ItemText = styled.div`
-  padding-top: 15vh;
-  text-align: center;
-`;
-
 export const Section = () => {
   return (
     <Wrap>
@@ -22,6 +8,45 @@ export const Section = () => {
         <h1>Model S</h1>
         <p>Order Online for Touchless Delivery</p>
       </ItemText>
+      <ButtonGroup>
+        <LeftButton>Customer Order</LeftButton>
+        <RightButton>Existing Inventory</RightButton>
+      </ButtonGroup>
     </Wrap>
   );
 };
+
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url('/images/model-s.jpg');
+  display: flex;
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+`;
+
+const LeftButton = styled.div`
+  width: 100%;
+  max-width: 260px;
+  background: rgba(23, 26, 32, 0.8);
+  color: #fff;
+  text-align: center;
+  white-space: nowrap;
+  padding: 10px 20px;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+`;
+
+const RightButton = styled(LeftButton)``;
